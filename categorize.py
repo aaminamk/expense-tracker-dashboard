b697da6bdb42d6ad3babefd837600eb7507301cf
aaminamk <128374046+aaminamk@users.noreply.github.com>
2025-11-10 17:06:05 +0500

    Update categorize.py

--- categorize.py
+++ categorize.py
@@ -1,18 +1,19 @@
 # categorize.py
 
 categories = {
-    "food": ["coffee", "tea", "restaurant", "burger", "drink", "pizza", "snack"],
-    "transport": ["bus", "taxi", "uber", "fuel", "gas", "train"],
-    "shopping": ["clothes", "cosmetics", "boots", "shoes", "bag"],
-    "education": ["course", "book", "subscription"],
-    "entertainment": ["cinema", "movies", "game"],
+    "Food": ["coffee", "tea", "restaurant", "burger", "drink", "pizza", "snack"],
+    "Transport": ["bus", "taxi", "uber", "fuel", "gas", "train"],
+    "Shopping": ["clothes", "cosmetics", "boots", "shoes", "bag"],
+    "Education": ["course", "book", "subscription"],
+    "Entertainment": ["cinema", "movies", "game"],
 }
 
-def categorize_expense(text):
+def categorize_expense(text: str) -> str:
+    """Categorizes expense based on keywords."""
     text = text.lower()
 
     for category, keywords in categories.items():
         if any(word in text for word in keywords):
-            return category.capitalize()
+            return category
 
     return "Other"
